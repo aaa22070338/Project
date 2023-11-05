4129eec297b247b00549ccc47b7a61ded5d676c2
noob <mike22070338@gmail.com>
2023-11-06 01:45:58 +0800

    [更新] 不再顯示YOLO模型偵測訊息

--- cube_detector/cube_detector.py
+++ cube_detector/cube_detector.py
@@ -10,7 +10,7 @@ class CubeDetector:
         self.model = cube_model
         self.suface_model = cube_surface_model
 
-    def detect(self, img:cv2.UMat, index:int|None=None, color: str | None= None, show_process_img=False,show_text=True):
+    def detect(self, img:cv2.UMat, index:int|None=None, color: str | None= None, show_process_img=False,show_text=True,method=None):
         self.img = img
         self.output_img = img.copy()
         self.cube_image_points={}
@@ -18,6 +18,7 @@ class CubeDetector:
         self.finding_color = color
         self.show_img_process=show_process_img
         self.show_text = show_text
+        self.detect_method = method
 
         cube_results = self.model(self.img)[0]
 
@@ -41,9 +42,11 @@ class CubeDetector:
             color_detected, color_rgb = self.__color_detect(masked_image)
             if self.finding_color != None and not color_detected == self.finding_color:
                 raise DetectError("未偵測到所要搜尋顏色")
-            corner_image,corner_points =  self.__conner_detect(masked_image, color_rgb, self.show_img_process)
-            plane_corners = self.__largest_plane_detect(corner_image,corner_points,self.show_img_process,self.show_text)
-            self.cube_image_points[color_detected]=plane_corners
+            corner_image,corner_points =  self.__conner_detect(masked_image, color_rgb)
+            plane_corners = self.__largest_plane_detect(corner_image,corner_points)
+
+            if box.conf.cpu() >0.8:
+                self.cube_image_points[color_detected]=plane_corners
         except DetectError as Error:
             print(Error)
 
@@ -102,7 +105,7 @@ class CubeDetector:
             raise DetectError("無法判斷顏色")
         return max_color, color_dict[max_color]
 
-    def __conner_detect(self, masked_image, color_rgb, show_img_process):
+    def __conner_detect(self, masked_image, color_rgb):
         result2 = self.suface_model(self.img)
         masks = result2[0].masks
         boxes = result2[0].boxes
@@ -131,25 +134,12 @@ class CubeDetector:
             epsilon = 0.025 * cv2.arcLength(contour,isClosed)
             approx_points = cv2.approxPolyDP(contour,epsilon,isClosed)
             approx_points_pack.append(approx_points)
-
-        if show_img_process:
-            contour_image = np.zeros(
-                (masked_image.shape[0], masked_image.shape[1], 3), dtype=np.uint8
-            )
-            contour_image = cv2.drawContours(contour_image, contours, -1, (0, 0, 255), 1)
-
+        
         if len(approx_points_pack)==1 or np.any(approx_points_pack is None):
             raise DetectError("[面]輪廓抓取近似多邊形失敗")
-        if show_img_process:
-            approx_image = np.zeros(
-                (masked_image.shape[0], masked_image.shape[1]), dtype=np.uint8
-            )
-            for approx_points in approx_points_pack:
-                approx_image = cv2.polylines(approx_image, [approx_points.astype(int)], isClosed, (255), 2)
-
         # 將打包的多邊形端點整理校正位置，首項為校正後的多邊形，次項為校正後的各個座標點
         correct_approx_points_pack, updated_points = correct_coordinates(
-            approx_points_pack, epsilon=self.epsilon_outer
+            copy.deepcopy(approx_points_pack), epsilon=self.epsilon_outer
         )
         if correct_approx_points_pack is None:
             raise DetectError("[面]輪廓聚類失敗")
@@ -163,42 +153,41 @@ class CubeDetector:
                 correct_approx_image, [correct_approx_points.astype(int)], isClosed, (255), 2
             )
 
-        if show_img_process:
-            for point in updated_points:
-                x, y = np.intp(point)
-                cv2.circle(masked_image, (x, y), 5, color_rgb, -1)
-
         for point in updated_points:
             x,y=np.intp(point)
             cv2.circle(self.output_img, (x, y), 5, color_rgb, -1)
 
-        if show_img_process:
+
+        if self.show_img_process:
+            contour_image = np.zeros(
+                (masked_image.shape[0], masked_image.shape[1], 3), dtype=np.uint8
+            )
+            contour_image = cv2.drawContours(contour_image, contours, -1, (0, 0, 255), 1)
+
+            approx_image = np.zeros(
+                (masked_image.shape[0], masked_image.shape[1]), dtype=np.uint8
+            )
+            for approx_points in approx_points_pack:
+                approx_image = cv2.polylines(approx_image, [approx_points.astype(int)], isClosed, (255), 2)
+            for point in updated_points:
+                x, y = np.intp(point)
+                cv2.circle(masked_image, (x, y), 5, color_rgb, -1)
+            
             cv2.imshow("origin", self.output_img)
             cv2.imshow("masked", masked_image)
             cv2.imshow("contour", contour_image)
             cv2.imshow("approx", approx_image)
             cv2.imshow("correct approx", correct_approx_image)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
         return correct_approx_image,updated_points
     
-    def __largest_plane_detect(self,corner_image,corner_points,show_img_process=False,show_text=True):
+    def __largest_plane_detect(self,corner_image,corner_points):
         if corner_image is None or corner_points is None:
             # return None
             raise DetectError("[方塊角點]抓取失敗")
-
         # 抓修正後的方塊圖，掃描內外輪廓，去除外輪廓得到各個面的面輪廓，並依據面積大小進行排序
         contours,_ = cv2.findContours(corner_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         contours = contours[1:]
         contours = sorted(contours,key=cv2.contourArea,reverse=True)
-
-        contour_image = np.zeros(
-                (corner_image.shape[0], corner_image.shape[1], 3), dtype=np.uint8
-            )
-        contour_image = cv2.drawContours(
-            contour_image, contours, -1, (0, 0, 255), 1
-        )
-
         # 抓出近似四邊形後的輪廓
         contours_approx = [contour_approx for contour_approx in [cv2.approxPolyDP(contour, 0.04 * cv2.arcLength(contour, True), True) for contour in contours ] if len(contour_approx)==4]
         # 確認面輪廓是否存在
@@ -208,43 +197,47 @@ class CubeDetector:
         # 若只有一個四角面輪廓則使用四點座標標定法，否則六點座標標定
         draw_squence_points = None
         _ , contour_touch_points = correct_coordinates(copy.deepcopy(contours_approx),self.epsilon_outer)
-        if (not contour_touch_points is None) and len(contour_touch_points)>=1:
+        if contour_touch_points is not None and len(contour_touch_points)>=1:
             contour_touch_points = np.array([[point] for point in contour_touch_points])
             draw_squence_points= self.__grab_six_points(contours_approx,contour_touch_points)
-        if not draw_squence_points is None:
-            if show_text:
+        if draw_squence_points is not None:
+            if self.show_text:
                 for i , point in enumerate(draw_squence_points):
                     cv2.putText(self.output_img, f"{i}", list(np.intp(point)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2)
             # 真正的角點為前面抓角點處理所得到的角點，所以將紀錄座標更新成最接近的角點
             closest_indices = np.argmin(np.linalg.norm(draw_squence_points[:, np.newaxis, :] - corner_points, axis=2), axis=1)
             draw_squence_points = corner_points[closest_indices]
             return np.array(draw_squence_points)
-
         # 四點，面座標標定，當無法檢測到六點位置時使用
         draw_squence_points=[]
         coordinates=[(0,0,0),(0,1,0),(1,1,0),(1,0,0)]
         for point, coordinate in zip(contours_approx[0],coordinates):
             # 真正的角點為前面抓角點處理所得到的角點，所以將紀錄座標更新成最接近的角點
-            target_point = np.array(point)
-            distances = np.linalg.norm(corner_points - target_point, axis=1)
-            nearest_index = np.argmin(distances)
-            point = corner_points[nearest_index]
+            point=min(corner_points, key= lambda point: np.linalg.norm(point-np.array(point)))
             draw_squence_points.append(point)
             # 將標點情況繪製於圖像，方便檢查
-            if show_text:
+            if self.show_text:
                 cv2.putText(self.output_img, f"{coordinate}", list(np.intp(point)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-        if contours!=[]:
-            # 顯示最大方塊塊輪廓面
-            cv2.fillPoly(contour_image, [contours[0]], (255, 0, 0)) 
+        if self.show_img_process:
+            contour_image = np.zeros(
+                (corner_image.shape[0], corner_image.shape[1], 3), dtype=np.uint8
+            )
             contour_image = cv2.drawContours(
+                contour_image, contours, -1, (0, 0, 255), 1
+            )
+            if contours!=[]:
+            # 顯示最大方塊塊輪廓面
+                cv2.fillPoly(contour_image, [contours[0]], (255, 0, 0)) 
+                contour_image = cv2.drawContours(
                     contour_image, contours, -1, (0, 0, 255), 1
                 ) 
-        if show_img_process:
             cv2.imshow("contour", contour_image)
             # cv2.waitKey(0)
         draw_squence_points = np.array(draw_squence_points)
         return draw_squence_points
+    
+
     def __grab_six_points(self,contours_approx,contour_touch_points):
         # 創建空陣列準備紀錄座標順序
         draw_squence_points=np.empty((0,2))
@@ -273,6 +266,7 @@ class CubeDetector:
             print(f"{draw_squence_points=}")
             draw_squence_points[4:,:] = contour[1:3,:]                    
         return np.array(draw_squence_points)
+    
 
 class DetectError(Exception):
     def __init__(self, message):
